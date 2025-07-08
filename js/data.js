d01fd05f4162d8d2ad507d926fbf539b84958567
Phennylalanine <petergregoryj@gmail.com>
2025-07-08 09:24:52 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 
 export async function loadVerbs() {
   try {
-    const response = await fetch('/data/verbs.json');
+    const response = await fetch('data/verbs.json');
     if (!response.ok) {
       throw new Error('Failed to load verbs.json');
     }
